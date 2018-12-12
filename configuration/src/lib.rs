03bb7b4e844591d30440d3684ad78d69cc691203
Augusto Teixeira <augusto@cartesi.io>
2018-12-12 21:55:39 -0200

    Start implementing config

--- configuration/src/lib.rs
+++ configuration/src/lib.rs
@@ -1,10 +1,29 @@
-extern crate ethabi;
+//extern crate ethabi;
+
+const DEFAULT_MAX_DELAY: u64 = 100;
 
 pub struct Concern {
     contract: String,
     user: String,
 }
 
+//#[macro_use]
+//extern crate serde_derive;
+
+//#[derive(Serialize, Deserialize)]
 pub struct Configuration {
-    concerns: Vec<Concern>,
+    max_delay: Option<u64>,
+    pub concerns: Vec<Concern>,
+}
+
+impl Configuration {
+    pub fn new(path: &str) -> Configuration {
+        Configuration {
+            max_delay: None,
+            concerns: vec![],
+        }
+    }
+    pub fn get_max_delay(&self) -> u64 {
+        self.max_delay.unwrap_or(DEFAULT_MAX_DELAY)
+    }
 }
--- dispatcher/src/main.rs
+++ dispatcher/src/main.rs
@@ -1,12 +1,19 @@
+extern crate configuration;
 extern crate utils;
 extern crate web3;
 
+use configuration::Configuration;
 use utils::EthExt;
 use web3::futures::Future;
 
 fn main() {
-    let (_eloop, transport) =
-        web3::transports::Http::new("http://35.231.249.221:8545").unwrap();
+    //let url = "http://35.231.249.221:8545"; // remote parity
+    let url = "http://127.0.0.1:8545"; // ganache
+
+    let config = Configuration::new("bla");
+    println!("Max delay: {}", config.get_max_delay());
+
+    let (_eloop, transport) = web3::transports::Http::new(url).unwrap();
     let web3 = web3::Web3::new(transport);
     let accounts = web3.eth().accounts().wait().unwrap();
 
