21cad3386d7640b819a18547176a4325d9aeec13
Augusto Teixeira <augusto@cartesi.io>
2018-12-23 16:34:41 -0200

    One concern hadled by env and cli options

--- error/src/lib.rs
+++ error/src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate error_chain;
+extern crate envy;
 extern crate serde_yaml;
 extern crate web3;
 
@@ -7,14 +8,15 @@ error_chain!{
     foreign_links {
         Io(::std::io::Error) #[cfg(unix)];
         Parsing(serde_yaml::Error);
+        Env(envy::Error);
     }
     links {
         Web3(web3::error::Error, web3::error::ErrorKind) #[cfg(unix)];
     }
     errors {
-        InvalidConfig {
-            description("invalid max_delay and warn_delay")
-                display("both max_delay and warn_delay must be set and valid")
+        InvalidConfig(details: String) {
+            description("invalid configuration")
+                display("invalid configuration: {}", details)
         }
         ChainNotInSync(delay: i64, max_delay: u64) {
             description("chain too delayed")
