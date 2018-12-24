6a53500fd7e9b38cce660e44b9a3b60169d03009
Augusto Teixeira <augusto@impa.br>
2018-12-23 22:58:12 -0200

    Structure configuration bettern

--- configuration/src/lib.rs
+++ configuration/src/lib.rs
@@ -19,12 +19,14 @@ use std::fs::File;
 use std::io::Read;
 use structopt::StructOpt;
 
+/// A concern is a pair: smart contract and user
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Concern {
     contract: String,
     user: String,
 }
 
+/// Structure for parsing configurations, both Environment and CLI arguments
 #[derive(StructOpt, Deserialize, Debug)]
 #[structopt(name = "basic")]
 struct EnvCLIConfiguration {
@@ -48,6 +50,7 @@ struct EnvCLIConfiguration {
     concern_user: Option<String>,
 }
 
+/// Structure to parse configuration from file
 #[derive(Serialize, Deserialize, Debug)]
 struct FileConfiguration {
     url: Option<String>,
@@ -57,6 +60,7 @@ struct FileConfiguration {
     concerns: Vec<Concern>,
 }
 
+/// Configuration after parsing
 #[derive(Debug)]
 pub struct Configuration {
     pub url: String,
@@ -67,11 +71,14 @@ pub struct Configuration {
 }
 
 impl Configuration {
+    /// Creates a Configuration from a file as well as the Environment
+    /// and CLI arguments
     pub fn new(path: &str) -> Result<Configuration> {
-        // tries to load config from CLI arguments
+        // try to load config from CLI arguments
         let cli_config = EnvCLIConfiguration::from_args();
         info!("CLI args: {:?}", cli_config);
         println!("Bla");
+
         // try to load config from env
         let env_config = envy::from_env::<EnvCLIConfiguration>()?;
         info!("Env args: {:?}", env_config);
@@ -91,94 +98,17 @@ impl Configuration {
                     format!("could not parse configuration file: {}", path)
                 })?;
 
-        // determining url (cli -> env -> config)
-        let url: String = cli_config
-            .url
-            .or(env_config.url)
-            .or(file_config.url)
-            .ok_or(Error::from(ErrorKind::InvalidConfig(String::from(
-                "Need to provide url (config file, command line or env)",
-            ))))?;
-
-        // determine testing (cli -> env -> config)
-        let testing: bool = cli_config
-            .testing
-            .or(env_config.testing)
-            .or(file_config.testing)
-            .unwrap_or(false);
-
-        // determine max_delay (cli -> env -> config)
-        let max_delay = cli_config
-            .max_delay
-            .or(env_config.max_delay)
-            .or(file_config.max_delay)
-            .unwrap_or(DEFAULT_MAX_DELAY);
-
-        // determine warn_delay (cli -> env -> config)
-        let warn_delay = cli_config
-            .warn_delay
-            .or(env_config.warn_delay)
-            .or(file_config.warn_delay)
-            .unwrap_or(DEFAULT_WARN_DELAY);
-
-        // determine cli concern
-        let cli_concern_defined = cli_config.concern_contract.is_some()
-            || cli_config.concern_user.is_some();
-        let cli_concern = if cli_concern_defined {
-            Some(Concern {
-                contract: cli_config
-                    .concern_contract
-                    .expect("All fields of cli_concern should be specified"),
-                user: cli_config
-                    .concern_user
-                    .expect("All fields of cli_concern should be specified"),
-            })
-        } else {
-            None
-        };
-
-        // determine env concern
-        let env_concern_defined = env_config.concern_contract.is_some()
-            || env_config.concern_user.is_some();
-        let env_concern = if env_concern_defined {
-            Some(Concern {
-                contract: env_config
-                    .concern_contract
-                    .expect("All fields of env_concern should be specified"),
-                user: env_config
-                    .concern_user
-                    .expect("All fields of env_concern should be specified"),
-            })
-        } else {
-            None
-        };
-
-        // determine concerns
-        let mut concerns = file_config.concerns;
-        if let Some(c) = cli_concern {
-            concerns.push(c);
-        };
-        if let Some(c) = env_concern {
-            concerns.push(c);
-        };
-
-        let config = Configuration {
-            url: url,
-            testing: testing,
-            max_delay: max_delay,
-            warn_delay: warn_delay,
-            concerns: concerns,
-        };
-
+        // merge these three configurations
+        let config = combine_config(cli_config, env_config, file_config)?;
         info!("Combined args: {:?}", config);
 
+        // check if max_delay and warn delay are compatible
         if config.max_delay < config.warn_delay {
             return Err(Error::from(ErrorKind::InvalidConfig(
                 "max_delay should be larger than warn delay".into(),
             )));
         }
 
-        //println!("{}", config);
         Ok(config)
     }
 }
@@ -199,4 +129,87 @@ impl fmt::Display for Configuration {
             self.concerns.len()
         )
     }
+}
+
+/// check if a given concern is well formed (either having all arguments
+/// or none).
+fn validate_concern(
+    contract: Option<String>,
+    user: Option<String>,
+) -> Result<Option<Concern>> {
+    // if some option is Some, both should be
+    if contract.is_some() || user.is_some() {
+        Ok(Some(Concern {
+            contract: contract.ok_or(Error::from(ErrorKind::InvalidConfig(
+                String::from("All fields of cli_concern should be specified"),
+            )))?,
+            user: user.ok_or(Error::from(ErrorKind::InvalidConfig(
+                String::from("All fields of cli_concern should be specified"),
+            )))?,
+        }))
+    } else {
+        Ok(None)
+    }
+}
+
+/// Combines the three configurations from: CLI, Environment and file.
+fn combine_config(
+    cli_config: EnvCLIConfiguration,
+    env_config: EnvCLIConfiguration,
+    file_config: FileConfiguration,
+) -> Result<Configuration> {
+    // determine url (cli -> env -> config)
+    let url: String = cli_config
+        .url
+        .or(env_config.url)
+        .or(file_config.url)
+        .ok_or(Error::from(ErrorKind::InvalidConfig(String::from(
+            "Need to provide url (config file, command line or env)",
+        ))))?;
+
+    // determine testing (cli -> env -> config)
+    let testing: bool = cli_config
+        .testing
+        .or(env_config.testing)
+        .or(file_config.testing)
+        .unwrap_or(false);
+
+    // determine max_delay (cli -> env -> config)
+    let max_delay = cli_config
+        .max_delay
+        .or(env_config.max_delay)
+        .or(file_config.max_delay)
+        .unwrap_or(DEFAULT_MAX_DELAY);
+
+    // determine warn_delay (cli -> env -> config)
+    let warn_delay = cli_config
+        .warn_delay
+        .or(env_config.warn_delay)
+        .or(file_config.warn_delay)
+        .unwrap_or(DEFAULT_WARN_DELAY);
+
+    // determine cli concern
+    let cli_concern =
+        validate_concern(cli_config.concern_contract, cli_config.concern_user)?;
+
+    // determine env concern
+    let env_concern =
+        validate_concern(env_config.concern_contract, env_config.concern_user)?;
+
+    // determine concerns (start from file and push CLI and Environment)
+    let mut concerns = file_config.concerns;
+    if let Some(c) = cli_concern {
+        concerns.push(c);
+    };
+    if let Some(c) = env_concern {
+        concerns.push(c);
+    };
+
+    Ok(Configuration {
+        url: url,
+        testing: testing,
+        max_delay: max_delay,
+        warn_delay: warn_delay,
+        concerns: concerns,
+    })
 }
