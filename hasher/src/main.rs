dca4d007e662bf15931bacf012d37f8ea8a468a5
felipeargento <felipe@cartesi.io>
2019-08-13 23:57:54 +0800

    Add license header to source code

--- hasher/src/main.rs
+++ hasher/src/main.rs
@@ -1,3 +1,29 @@
+// Dispatcher provides the infrastructure to support the development of DApps,
+// mediating the communication between on-chain and off-chain components. 
+
+// Copyright (C) 2019 Cartesi Pte. Ltd.
+
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// Note: This component currently has dependencies that are licensed under the GNU
+// GPL, version 3, and so you should treat this component as a whole as being under
+// the GPL version 3. But all Cartesi-written code in this component is licensed
+// under the Apache License, version 2, or a compatible permissive license, and can
+// be used independently under the Apache v2 license. After this component is
+// rewritten, the entire component will be released under the Apache v2 license.
+
+
+
 extern crate env_logger;
 extern crate futures;
 extern crate futures_cpupool;
